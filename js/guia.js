f378c8e13d278e9d76d57acd3d59475769af69fd
bcnitb <bcn4itb@gmail.com>
2022-05-12 11:08:56 +0200

    Arbre de decisions creat

---
+++ js/guia.js
@@ -0,0 +1,10 @@
+$(document).on('keydown', function(event) {  
+    if(event.which == 33) { // F7
+      $("#voice").click();
+    }
+  });
+  $("#left").on('click', function() {
+    alert('hello');
+  });
+
+  
